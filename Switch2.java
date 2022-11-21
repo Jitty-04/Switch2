89e0fcf41e39de435745aa4d98c630523b8da68f
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-21 12:33:14 +0530

    push

--- Switch2.java
+++ Switch2.java
@@ -1,5 +1,16 @@
 public class Switch2 {
-    public static void main (String[] s){
+   public static void main(String[] arg) {
+       while (true) {
+           System.out.println("1:Largest among 3 numbers");
+           System.out.printl("2:Sallest among 3 numbers");
+           System.out.printl("3:Check prime or not");
+           System.out.printl("4:Even or not");
+           System.out.println("5:Reverse of a number");
+           System.out.printlin("6:Exit");
+           int c=sc.nextInt();
+           switch (c) {
 
-    }
+           }
+       }
+   }
 }
